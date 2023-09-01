9a2d73612529bfe908ce0b79e3dd7f058a034393
glenio-juste <glenio.juste@gmail.com>
2023-09-01 19:35:33 -0300

    Método listar

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<Medico> getAll() {
-        return repository.findAll();
+    public List<DadosListagemMedico> listar() {
+
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
     @GetMapping("/{id}")
