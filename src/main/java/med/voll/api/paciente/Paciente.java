0d265e300816e7f4ba1632de1ffac70d84b34406
glenio-juste <glenio.juste@gmail.com>
2023-09-09 17:47:52 -0300

    Adicionando a classe Paciente

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,30 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.Especialidade;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+}
