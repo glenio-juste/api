02f24e68eea84acd8c80c12a03856d80e0e87c17
glenio-juste <glenio.juste@gmail.com>
2023-09-12 20:36:41 -0300

    tratarErro400

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,22 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+
+        var erros = ex.getFieldErrors();
+        // erros me dá um stream e mapeie cada obj de filderro para um obj DadosErroValidacao
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList()); // o new vai chamar um construtor e passar para uma lista
+                                           // conversão de uma lista para outra
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 
 }
