1175dee97a0209d36f13150ed8c6cd7dd521ebd7
glenio-juste <glenio.juste@gmail.com>
2023-10-24 20:16:20 -0300

    Validando o token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,13 +13,17 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("Filtro CHAMADO!!!");
 
         var tokenJWT = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJWT);
 
-        System.out.println(tokenJWT);
+        System.out.println(subject);
 
         filterChain.doFilter(request, response);
     }
@@ -31,7 +36,7 @@ public class SecurityFilter extends OncePerRequestFilter {
             throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
         }
 
-        return authorizationHeader.replace("Bearer ", "");
+        return authorizationHeader.replace("Bearer ", "").trim();
 
     }
 
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario){
         try {
-            var algoritmo = Algorithm.HMAC256(secret);
+            var algoritmo = Algorithm.HMAC256(secret); // create algorithm
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
@@ -29,6 +30,24 @@ public class TokenService {
             throw new RuntimeException("Erro ao gerar token JWT: ", exception);
         }
     }
+
+
+    public String getSubject(String tokenJWT){
+
+        try {
+            var algoritmo = Algorithm.HMAC256(secret); // create algorithm
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+
+    }
+
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
