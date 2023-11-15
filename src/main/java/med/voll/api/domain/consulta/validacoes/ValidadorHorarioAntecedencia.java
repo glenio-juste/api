2bfc64406e6a0f52fe57f665a5a9fb46415927c7
glenio-juste <glenio.juste@gmail.com>
2023-11-15 19:30:38 -0300

    ValidadorHorarioAntecedencia

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,25 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.ValidacaoException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var dataConsulta = dados.data();
+
+        var agora = LocalDateTime.now();
+
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos!");
+        }
+
+    }
+
+}
