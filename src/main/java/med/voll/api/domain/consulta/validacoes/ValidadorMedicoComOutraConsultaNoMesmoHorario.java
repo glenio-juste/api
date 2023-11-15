3d42469d1a4f28866739cca1cc01dbbc80c3e06e
glenio-juste <glenio.juste@gmail.com>
2023-11-15 20:03:26 -0300

    ajuste ValidadorMedicoComOutraConsultaNoMesmoHorario

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -12,7 +12,7 @@ public class ValidadorMedicoComOutraConsultaNoMesmoHorario {
 
         //Não permitir o agendamento de uma consulta com um médico que já possui outra consulta agendada na mesma data/hora
 
-        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico());
+        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
 
         if(medicoPossuiOutraConsultaNoMesmoHorario){
             throw new ValidacaoException("Médico já possui outra consulta agendada nesse mesmo horário");
